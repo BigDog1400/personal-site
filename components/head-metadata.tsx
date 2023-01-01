33cbe71cf1a794947e86a5e68a5c7812c6a528f6
BigDog1400 <const.isaac@gmail.com>
2023-01-01 08:29:29 -0400

    feat: update seo meta

--- components/head-metadata.tsx
+++ components/head-metadata.tsx
@@ -12,19 +12,18 @@ export function HeadMetadata({
   title = "Isaac - Desarrollador web",
   description = "Un blog sobre desarrollo web, react, typescript, etc.",
   image = "",
-  url = "",
+  url = process.env.NEXT_PUBLIC_WEBSITE_URL,
 }: HeadMetadataProps) {
-  console.log("title", title);
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image} />
-      <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
+      <meta name="og:title" content={title} key="ogtitle" />
+      <meta name="og:description" content={description} key="ogdesc" />
       <meta name="og:image" content={image} />
-      <meta name="og:url" content={url} />
-      <meta name="og:type" content="website" />
+      <meta name="og:url" content={url} key="ogurl" />
+      <meta name="og:type" content="website" key="ogtype" />
       <meta name="twitter:site" content="@log1400" />
       <meta name="twitter:creator" content="@log1400" />
       <meta name="twitter:title" content={title} />
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CommonLayoutPage } from "../../components/common-layout";
 import { getPosts } from "../api/get-blogs";
 import Link from "next/link";
+import { HeadMetadata } from "../../components/head-metadata";
 
 export const getStaticProps = () => {
   const posts = getPosts(); // the argument has no effect yet
@@ -30,49 +31,54 @@ function BlogPage({
   }>;
 }) {
   return (
-    <CommonLayoutPage>
-      <div className="max-w-4xl mx-auto flex flex-col pt-4 sm:pt-10  md:px-0 px-5">
-        <h1 className="text-3xl text-white font-bold">
-          Blog
-          <span className="text-teal-600">📝</span>
-        </h1>
-        <div className="pt-4">
-          {posts.map((post) => (
-            <Link href={`/blog/${post.slug}`} key={post.slug}>
-              <div className="md:p-5 rounded-lg cursor-pointer hover:bg-gray-900">
-                <h2 className="text-2xl font-bold  text-teal-600">
-                  {post.data.title}
-                </h2>
-                <p className="mt-4  text-[#d4d4d4]">{post.data.description}</p>
-                <div className="mt-4 flex flex-wrap gap-y-2 items-center">
-                  {post.data.tags.split(",").map((tag) => (
-                    <span
-                      key={tag}
-                      className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-                    >
-                      #{tag}
-                    </span>
-                  ))}
+    <>
+      <HeadMetadata url={process.env.NEXT_PUBLIC_WEBSITE_URL + "/blog"} />
+      <CommonLayoutPage>
+        <div className="max-w-4xl mx-auto flex flex-col pt-4 sm:pt-10  md:px-0 px-5">
+          <h1 className="text-3xl text-white font-bold">
+            Blog
+            <span className="text-teal-600">📝</span>
+          </h1>
+          <div className="pt-4">
+            {posts.map((post) => (
+              <Link href={`/blog/${post.slug}`} key={post.slug}>
+                <div className="md:p-5 rounded-lg cursor-pointer hover:bg-gray-900">
+                  <h2 className="text-2xl font-bold  text-teal-600">
+                    {post.data.title}
+                  </h2>
+                  <p className="mt-4  text-[#d4d4d4]">
+                    {post.data.description}
+                  </p>
+                  <div className="mt-4 flex flex-wrap gap-y-2 items-center">
+                    {post.data.tags.split(",").map((tag) => (
+                      <span
+                        key={tag}
+                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+                      >
+                        #{tag}
+                      </span>
+                    ))}
+                  </div>
+                  <p className="mt-4  text-[#d4d4d4]">
+                    {new Date(post.data.date).toLocaleDateString("es-us", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}{" "}
+                    •{" "}
+                    {new Intl.NumberFormat("es-us", {
+                      style: "unit",
+                      unit: "minute",
+                      unitDisplay: "long",
+                    }).format(Number(post.readTime))}
+                  </p>
                 </div>
-                <p className="mt-4  text-[#d4d4d4]">
-                  {new Date(post.data.date).toLocaleDateString("es-us", {
-                    day: "numeric",
-                    month: "long",
-                    year: "numeric",
-                  })}{" "}
-                  •{" "}
-                  {new Intl.NumberFormat("es-us", {
-                    style: "unit",
-                    unit: "minute",
-                    unitDisplay: "long",
-                  }).format(Number(post.readTime))}
-                </p>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </CommonLayoutPage>
+      </CommonLayoutPage>
+    </>
   );
 }
 
