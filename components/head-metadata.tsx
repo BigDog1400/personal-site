393eefb79f3f4724d82845a4520746c729749712
BigDog1400 <const.isaac@gmail.com>
2023-01-01 01:35:06 -0400

    feat: update head metadata

--- components/head-metadata.tsx
+++ components/head-metadata.tsx
@@ -22,6 +22,14 @@ export function HeadMetadata({
       <meta name="image" content={image} />
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
+      <meta name="og:image" content={image} />
+      <meta name="og:url" content={url} />
+      <meta name="og:type" content="website" />
+      <meta name="twitter:site" content="@log1400" />
+      <meta name="twitter:creator" content="@log1400" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
     </Head>
   );
 }
