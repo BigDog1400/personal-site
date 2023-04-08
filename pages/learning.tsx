01ceed7a76c03ae6393567f5cdc2615bed08789c
BigDog1400 <const.isaac@blindcreator.com>
2023-04-08 00:27:09 -0400

    fix a tag

--- pages/learning.tsx
+++ pages/learning.tsx
@@ -14,19 +14,21 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 xl:grid-cols-3">
           <div className="flex flex-col p-4 bg-white rounded-lg shadow">
             <h2 className="mb-4 text-lg font-medium text-gray-900">Curso de HTML</h2>
-            <a href="https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/" target="_blank" className="text-blue-600 hover:text-blue-800">
+            <a 
+              rel="noreferrer"
+            href="https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/" target="_blank" className="text-blue-600 hover:text-blue-800">
               Ir al curso &rarr;
             </a>
           </div>
           <div className="flex flex-col p-4 bg-white rounded-lg shadow">
             <h2 className="mb-4 text-lg font-medium text-gray-900">Curso de Javascript</h2>
-            <a href="https://www.udemy.com/course/the-complete-javascript-course/" target="_blank" className="text-blue-600 hover:text-blue-800">
+            <a rel="noreferrer" href="https://www.udemy.com/course/the-complete-javascript-course/" target="_blank" className="text-blue-600 hover:text-blue-800">
               Ir al curso &rarr;
             </a>
           </div>
           <div className="flex flex-col p-4 bg-white rounded-lg shadow">
             <h2 className="mb-4 text-lg font-medium text-gray-900">Curso de React</h2>
-            <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/" target="_blank" className="text-blue-600 hover:text-blue-800">
+            <a rel="noreferrer" href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/" target="_blank" className="text-blue-600 hover:text-blue-800">
               Ir al curso &rarr;
             </a>
           </div>
