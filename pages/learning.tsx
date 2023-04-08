7b5ca5a14a6094818fc42124b6eba4f32063c670
BigDog1400 <const.isaac@blindcreator.com>
2023-04-08 00:31:58 -0400

    fix : add recommendation

--- pages/learning.tsx
+++ pages/learning.tsx
@@ -10,7 +10,11 @@ export default function Home() {
       <HeadMetadata />
       <CommonLayoutPage>
       <div className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold leading-tight text-gray-900">Mis cursos recomendados</h1>
+        <h1 className="text-3xl font-bold leading-tight text-white">Mis cursos recomendados</h1>
+        <h2 className="mt-4 text-lg font-medium text-gray-500">Estos son los cursos que yo mismo he realizado y que me han ayudado a aprender y mejorar mis habilidades como desarrollador web.</h2>
+        <h3 className="mt-4 text-lg font-medium text-gray-500">
+          Espera que esten en oferta, porque son cursos muy buenos y a veces tienen descuentos muy buenos.
+        </h3>
         <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 xl:grid-cols-3">
           <div className="flex flex-col p-4 bg-white rounded-lg shadow">
             <h2 className="mb-4 text-lg font-medium text-gray-900">Curso de HTML</h2>
